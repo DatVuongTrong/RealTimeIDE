8b86705faf9f6502617d8d69bcf8ee1d1f624723
Dat Vuong Trong <trongdatvuong@gmail.com>
2023-04-09 23:57:28 -0400

    handling errors

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const http = require('http');
 const {Server} = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server);
+const ACTIONS = require('./src/Action')
 
+const userSocketMap = {}
 io.on('connection', (socket) => {
-    console.log('socket connected', socket.id)
-})
+    console.log('socket connected ', socket.id)
+
+    socket.on(ACTIONS.JOIN, (roomId, username) => {
+        userSocketMap[socket.id] = username;
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
