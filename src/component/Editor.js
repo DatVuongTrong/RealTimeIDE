2974e995345a1a9f303f8ce1ca6ed5209ddb87b1
@DatVuongTrong <trongdatvuong@gmail.com>
2023-04-09 21:22:44 -0400

    Add server

--- src/component/Editor.js
+++ src/component/Editor.js
@@ -1,27 +1,26 @@
-import {React, useEffect } from 'react';
-import CodeMirror from 'codemirror';
-import 'codemirror/theme/dracula.css';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/lib/codemirror.css';
-
+import React, { useEffect } from "react";
+import CodeMirror from "codemirror";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/lib/codemirror.css";
 
 const Editor = () => {
   useEffect(() => {
-    async function intit() {
-      CodeMirror.fromTextArea(document.getElementById('realtimeEditor'), {
-        mode: {name: 'javascript', json: true },
-        theme: 'dracula',
+    async function init() {
+      CodeMirror.fromTextArea(document.getElementById("realtimeEditor"), {
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
         autoCloseTags: true,
-        autocloseBrackets: true,
+        autoCloseBrackets: true,
         lineNumbers: true,
       });
-    }
-    intit();
+    };
+    init();
   }, []);
-    return <textarea id="realtimeEditor"></textarea>;
-  
-}
+ 
+  return <textarea id="realtimeEditor"></textarea>;
+};
 
-export default Editor
+export default Editor;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,34 +1,49 @@
-import {React, useState} from "react";
+import { React, useState, useRef } from "react";
 
 import Client from "../component/Client";
 import Editor from "../component/Editor";
+import { initSocket } from "../../socket";
+import ACTIONS from "../../Action";
+
 
 const EditorPage = () => {
-  const [clients, setClients] = useState([{socketId: 1, username: 'Dat'}, {socketId: 2, username: 'Dat'}, {socketId: 2, username: 'Dat'}]);
+  const socketRef = useRef(null);
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.emit(ACTIONS.JOIN);
+    }
+    init();
+  }, [])
+
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Dat" },
+    { socketId: 2, username: "Dat" },
+    { socketId: 2, username: "Dat" },
+  ]);
   return (
     <div className="mainWrap">
       <div className="aside">
         <div className="asideInner">
           <div className="logo">
-            <img className="logoImage" src="/depauw_logo.png" alt="depauw_logo"></img>   
+            <img
+              className="logoImage"
+              src="/depauw_logo.png"
+              alt="depauw_logo"
+            ></img>
           </div>
           <h3>Connected</h3>
-          <div className="clientsList"> 
-            {
-              clients.map((client) => (
-                <Client key={client.socketId} username={client.username} />
-              ))
-            }
-
-            </div>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
         </div>
         <button className="btn copyBtn">Copy Room Id</button>
         <button className="btn leaveBtn">Leave</button>
-
-
       </div>
       <div className="editorWrap">
-      <Editor />
+        <Editor />
       </div>
     </div>
   );
